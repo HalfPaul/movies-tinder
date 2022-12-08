094f0b2013dc132827a5de482f2644a7f850d315
HalfPaul <pauliuspusk52@gmail.com>
2022-12-08 11:22:10 +0200

    Added .gitignore to api

--- api/main.py
+++ api/main.py
@@ -11,23 +11,6 @@ movie_indices = pd.read_csv("data/u.item", encoding='latin-1', delimiter='|',use
 
 
 app = FastAPI()
-if __name__ == '__main__':
-
-    from ml_model.nb_model import NB
-class CollaborativeFiltering(nn.Module):
-    def __init__(self, n_users, n_movies, n_factors):
-        super().__init__()
-        self.user_factors = nn.Embedding(n_users+1, n_factors)
-        self.user_bias = nn.Embedding(n_users+1, 1)
-        self.movie_factors = nn.Embedding(n_movies+1, n_factors)
-        self.movie_bias = nn.Embedding(n_movies+1, 1)
-    def forward(self, x):
-        embeddings = self.user_factors(x[:,0]), self.movie_factors(x[:,1])
-        out = (self.user_factors(x[:,0]) * self.movie_factors(x[:,1])).sum(dim=1)
-
-        out += self.user_bias(x[:,0]).squeeze(dim=1) + self.movie_bias(x[:,1]).squeeze(dim=1)
-        return out
-
 
 
 origins = [
