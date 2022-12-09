c982d0985dde0ddb76ac48e22e9412c7ff462ca9
HalfPaul <pauliuspusk52@gmail.com>
2022-12-09 17:29:09 +0200

    Card design done

--- client/pages/selection.tsx
+++ client/pages/selection.tsx
@@ -3,15 +3,23 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import React, {useEffect, useState} from 'react';
 import Card from "../components/Card";
+import axios from "axios";
 
 export default function Selection() {
-
+    const [likedIdxs, setLikedIdxs] = useState([])
+    const [currentMovie, setCurrentMovie] = useState("")
+    const [currentIdx, setCurrentIdx] = useState("");
     useEffect(() => {
-
+        const data = axios
+          .get("http://127.0.0.1:8000/getRandomMovie")
+          .then((res) => {
+            setCurrentMovie(res.data.title);
+            setCurrentIdx(res.data.movie_idx);
+          });
     }, [])
     return (
-    <div className="flex justify-center items-center w-full h-screen bg-gray-200">
-        <Card name="a" pictureLink="b"/>
-    </div>
+      <div className="flex justify-center items-center w-full h-screen bg-gray-200">
+        <Card name={currentMovie} idx={currentIdx} />
+      </div>
     );
 }
