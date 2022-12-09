e851a43e39b109ffa611f1553c70231cb48f8b14
HalfPaul <pauliuspusk52@gmail.com>
2022-12-09 17:59:33 +0200

    Implemented liking and disliking feature

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -1,23 +1,60 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { AiFillHeart, AiFillDislike } from "react-icons/ai";
 import { BsFillSkipForwardCircleFill } from "react-icons/bs"
 
 
-type CardProps = {
+/* type CardProps = {
     name: string
     idx: string
-}
+} */
 
-export default function Card({name, idx}: CardProps) {
+export default function Card() {
+  const [likedIdxs, setLikedIdxs] = useState<string[]>([]);
+  const [currentMovie, setCurrentMovie] = useState("");
+  const [currentIdx, setCurrentIdx] = useState("");
+
+  const [results, setResults] = useState({})
+
+  const getRandomMovie = () => {
+    axios
+      .get("http://127.0.0.1:8000/getRandomMovie")
+      .then((res) => {
+        setCurrentMovie(res.data.title);
+        setCurrentIdx(res.data.movie_idx);
+      });
+  }
+  const filmWasLiked = async () => {
+    
+    if (likedIdxs.length === 5) {
+      const query = likedIdxs.join("_")
+      const results = await axios.get(`http://127.0.0.1:8000/${query}`);
+      setResults(results.data.movies)
+    } else {
+      setLikedIdxs((likedIdxs) => [...likedIdxs, currentIdx]);
+    }
+  }
+  const filmWasNotLiked = () => {
+    if (likedIdxs.length !== 5) getRandomMovie();
+  };
+  useEffect(() => {
+    getRandomMovie();
+  }, [likedIdxs]);
   return (
-    <div className="flex bg-white mx-auto rounded-lg flex-col overflow-hidden max-w-sm">
-      <div className="flex flex-col text-center px-6 py-8">
-        <h5 className="font-bold text-2xl mb-2">{name}</h5>
+    <div className="flex bg-white mx-auto rounded-lg flex-col overflow-hidden max-w-sm w-[30%]">
+      {(Object.keys(results).length === 0) ?<div className="flex flex-col text-center px-6 py-8">
+        <h5 className="font-bold text-2xl mb-2">{currentMovie}</h5>
         <div className="flex flex-row justify-between mt-12">
-          <AiFillDislike size={60} color="red" />
-          <BsFillSkipForwardCircleFill size={60} color="gray"/>
-          <AiFillHeart size={60} color="green" />
+          <AiFillDislike size={60} color="red" onClick={filmWasNotLiked} />
+          <BsFillSkipForwardCircleFill
+            size={60}
+            color="gray"
+            onClick={filmWasNotLiked}
+          />
+          <AiFillHeart size={60} color="green" onClick={filmWasLiked} />
         </div>
-      </div>
+      </div> :
+      <div></div>}
     </div>
   );
 }
--- client/pages/selection.tsx
+++ client/pages/selection.tsx
@@ -6,20 +6,11 @@ import Card from "../components/Card";
 import axios from "axios";
 
 export default function Selection() {
-    const [likedIdxs, setLikedIdxs] = useState([])
-    const [currentMovie, setCurrentMovie] = useState("")
-    const [currentIdx, setCurrentIdx] = useState("");
-    useEffect(() => {
-        const data = axios
-          .get("http://127.0.0.1:8000/getRandomMovie")
-          .then((res) => {
-            setCurrentMovie(res.data.title);
-            setCurrentIdx(res.data.movie_idx);
-          });
-    }, [])
+    
+    
     return (
       <div className="flex justify-center items-center w-full h-screen bg-gray-200">
-        <Card name={currentMovie} idx={currentIdx} />
+        <Card />
       </div>
     );
 }
